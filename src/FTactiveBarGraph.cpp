ffd7aef32edae4e29c18ea24691d4b6d09e3cf49
essej <essej>
2003-02-16 23:02:11 +0000

    mod to fix pow call for some

--- src/FTactiveBarGraph.cpp
+++ src/FTactiveBarGraph.cpp
@@ -540,7 +540,7 @@ void FTactiveBarGraph::xToBinRange(int x, int &frombin, int &tobin)
 			float xscale = x / (float)_width;
 			
 			// use log scale for freq
-			lbin = rbin = (int) pow ( totbins>>1, xscale) - 1;
+			lbin = rbin = (int) ::pow ( (double) (totbins>>1), (double) xscale) - 1;
 			
 			// find lowest with same x
 			while ( (lbin > 0) && ((int)(((FTutils::fast_log10(lbin) / FTutils::fast_log10(totbins/2)) * _width)) == x)) {
@@ -568,7 +568,7 @@ void FTactiveBarGraph::xToBinRange(int x, int &frombin, int &tobin)
 			float xscale = x / (float)_width;
 			
 			// use log scale for freq
-			lbin = rbin = (int) pow ( (float)(totbins/3.0), xscale) - 1;
+			lbin = rbin = (int) ::pow ( (double)(totbins/3.0), xscale) - 1;
 			
 			// find lowest with same x
 			while ( (lbin > 0) && ((int)(((FTutils::fast_log10(lbin) / FTutils::fast_log10(totbins/3.0)) * _width)) == x)) {
